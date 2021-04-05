c1ae9b8d1fd70506966733c30672bb067fd32373
Pascal <pascal@bertschi.io>
2021-04-05 13:39:24 +0200

    fix program tests

--- tests/program.spec.ts
+++ tests/program.spec.ts
@@ -2,10 +2,29 @@ import { getProgram } from '../src/program'
 import { join } from 'path'
 
 describe('program', () => {
-  it('should get help with description', () => {
-    const fileName = join(__dirname, '../examples/local/build.yaml')
+  async function testCommand(args: string[]) {
+    const fileName = join(__dirname, '../examples/program/build.yaml')
     const program = getProgram(fileName)
-    const help = program.helpInformation({ error: false })
-    expect(help).toContain('example [options]  local shell example')
+    await new Promise<void>(async (resolve, reject) => {
+      try {
+        await program.exitOverride((err) => {
+          reject(err)
+        }).parseAsync([process.argv[0], fileName, ...args])
+        resolve()
+      } catch (e) {
+        reject(e);
+      }
+    })
+  }
+
+  it('should get help with description', () => {
+    const fileName = join(__dirname, '../examples/program/build.yaml')
+    const program = getProgram(fileName)
+    const help = program.exitOverride().helpInformation({ error: false })
+    expect(help).toContain('example [options]  install npm packages')
+  })
+
+  it('should run when cache is up to date and --no-cache arg is passed', async () => {
+    await testCommand(['example', '--no-cache'])
   })
 })
