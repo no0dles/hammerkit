22faea848afc05f61e23b0fd72d986c117d4ee17
Pascal <pascal@bertschi.io>
2021-04-05 13:47:24 +0200

    log to console for tests

--- tests/run-arg.ts
+++ tests/run-arg.ts
@@ -1,8 +1,16 @@
-import { RunArg } from '../src/run-arg'
-import consola from 'consola'
+import {RunArg} from '../src/run-arg'
+import consola, {LogLevel} from 'consola'
 
 export function getTestArg(): [RunArg, jest.Mock] {
   const mock = jest.fn()
-  consola.mock(() => mock)
-  return [new RunArg(false, 0), mock]
+  mock.mock
+  consola.mock(() => function (level, message) {
+    if (level && message) {
+      console.log(LogLevel[level], message)
+    }
+    return mock(...arguments)
+  })
+  const arg = new RunArg(false, 0)
+  arg.logger.level = LogLevel.Debug
+  return [arg, mock]
 }
