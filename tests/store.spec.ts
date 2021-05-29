b79ab5c616a4a973af3cc32dc0c3f37725bc21f9
Pascal Bertschi <pascal@bertschi.io>
2021-05-29 20:33:03 +0200

    fix cache restore test

--- tests/store.spec.ts
+++ tests/store.spec.ts
@@ -11,25 +11,30 @@ import { restore } from '../src/rewrite/7-restore'
 describe('store/restore', () => {
   const buildFile = loadExampleBuildFile('store-restore')
   const outputPath = join(buildFile.path, 'node_modules')
+  const cacheDir = join(buildFile.path, '.hammerkit')
   const storePath = join(tmpdir(), 'storetest')
 
   beforeEach(async () => {
     await remove(outputPath)
     await remove(storePath)
+    await remove(cacheDir)
   })
 
   it('should clean created outputs', async () => {
     const [arg] = getTestArg()
     await executeTask(buildFile, 'example', false, arg)
     expect(existsSync(outputPath)).toBeTruthy()
+    expect(existsSync(cacheDir)).toBeTruthy()
 
     await store(buildFile, storePath)
 
     await clean(buildFile)
     expect(existsSync(outputPath)).toBeFalsy()
+    expect(existsSync(cacheDir)).toBeFalsy()
 
     await restore(buildFile, storePath)
     expect(existsSync(outputPath)).toBeTruthy()
+    expect(existsSync(cacheDir)).toBeTruthy()
   })
 
   it('should not store anything if nothing got generated', async () => {
