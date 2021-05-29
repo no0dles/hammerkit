87d182bf87b7cbee317e8e0a159167070176be6d
Pascal Bertschi <pascal@bertschi.io>
2021-05-29 21:33:13 +0200

    optimize restore/store

--- src/rewrite/6-store.ts
+++ src/rewrite/6-store.ts
@@ -1,34 +1,25 @@
 import { join, relative } from 'path'
-import { remove } from '../file/remove'
-import { existsSync } from 'fs'
-import { copy } from '../file/copy'
 import { nodes } from './1-plan'
 import { ExecutionBuildFile } from './0-parse'
-import consola from 'consola'
+import { moveFiles } from './7-restore'
 
 export async function store(buildFile: ExecutionBuildFile, targetDirectory: string): Promise<void> {
   const tree = nodes(buildFile)
-  for (const key of Object.keys(tree)) {
-    const node = tree[key]
 
-    const cacheDir = join(node.path, '.hammerkit')
-    const targetCacheDir = join(targetDirectory, relative(buildFile.path, node.path), '.hammerkit')
-    if (existsSync(cacheDir) && !existsSync(targetCacheDir)) {
-      copy(cacheDir, targetCacheDir)
-    }
+  await moveFiles(function* () {
+    for (const key of Object.keys(tree)) {
+      const node = tree[key]
 
-    for (const sourcePath of node.generates) {
-      const relativePath = relative(buildFile.path, sourcePath)
-      const targetPath = join(targetDirectory, relativePath)
+      const cacheDir = join(node.path, '.hammerkit')
+      const sourceCacheDir = join(targetDirectory, relative(buildFile.path, node.path), '.hammerkit')
 
-      await remove(targetPath)
+      yield { from: cacheDir, to: sourceCacheDir }
 
-      if (!existsSync(sourcePath)) {
-        continue
+      for (const sourcePath of node.generates) {
+        const relativePath = relative(buildFile.path, sourcePath)
+        const targetPath = join(targetDirectory, relativePath)
+        yield { from: sourcePath, to: targetPath }
       }
-
-      consola.debug(`copy ${sourcePath} to ${targetPath}`)
-      copy(sourcePath, targetPath)
     }
-  }
+  })
 }
--- src/rewrite/7-restore.ts
+++ src/rewrite/7-restore.ts
@@ -1,35 +1,56 @@
 import { join, relative } from 'path'
 import { existsSync } from 'fs'
 import { copy } from '../file/copy'
-import { remove } from '../file/remove'
 import { nodes } from './1-plan'
 import { ExecutionBuildFile } from './0-parse'
 import consola from 'consola'
+import { remove } from '../file/remove'
 
 export async function restore(buildFile: ExecutionBuildFile, targetDirectory: string): Promise<void> {
   const tree = nodes(buildFile)
-  for (const key of Object.keys(tree)) {
-    const node = tree[key]
 
-    const cacheDir = join(node.path, '.hammerkit')
-    const sourceCacheDir = join(targetDirectory, relative(buildFile.path, node.path), '.hammerkit');
-    if (existsSync(sourceCacheDir) && !existsSync(cacheDir)) {
-      copy(sourceCacheDir, cacheDir)
+  await moveFiles(function* () {
+    for (const key of Object.keys(tree)) {
+      const node = tree[key]
+
+      const cacheDir = join(node.path, '.hammerkit')
+      const sourceCacheDir = join(targetDirectory, relative(buildFile.path, node.path), '.hammerkit')
+
+      yield { from: sourceCacheDir, to: cacheDir }
+
+      for (const targetPath of node.generates) {
+        const sourcePath = join(targetDirectory, relative(buildFile.path, targetPath))
+        yield { from: sourcePath, to: targetPath }
+      }
+    }
+  })
+}
+
+export async function moveFiles(folder: () => Generator<{ from: string; to: string }>) {
+  const foldersToCopy: { from: string; to: string }[] = []
+
+  const addFolder = (from: string, to: string) => {
+    if (!existsSync(from)) {
+      return
     }
 
-    for (const targetPath of node.generates) {
-      const sourcePath = join(targetDirectory, relative(buildFile.path, targetPath))
-
-      if (!existsSync(sourcePath)) {
-        continue
-      }
-
-      if (existsSync(targetPath)) {
-        await remove(targetPath)
-      }
-
-      consola.debug(`copy ${sourcePath} to ${targetPath}`)
-      copy(sourcePath, targetPath)
+    if (foldersToCopy.some((f) => f.from === from && f.to === to)) {
+      return
     }
+
+    foldersToCopy.push({ from, to })
+  }
+
+  for (const { from, to } of folder()) {
+    addFolder(from, to)
+  }
+
+  for (const folder of foldersToCopy) {
+    if (existsSync(folder.to)) {
+      await remove(folder.to)
+    }
+
+    consola.debug(`copy ${folder.from} to ${folder.to}`)
+    copy(folder.from, folder.to)
   }
 }
